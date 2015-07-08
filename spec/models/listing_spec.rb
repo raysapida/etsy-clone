9241b46238e85b51bb2deafd0cdcbae16b4b3fed
Raymond Sapida <ray.sapida@gmail.com>
2015-07-08 01:24:06 -0700

    Add validations to listing and user model

--- spec/models/listing_spec.rb
+++ spec/models/listing_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Listing, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should validate_presence_of :name }
+  it { should validate_presence_of :description }
+  it { should validate_presence_of :price }
+  it { should validate_numericality_of(:price).is_greater_than(0) }
 end
