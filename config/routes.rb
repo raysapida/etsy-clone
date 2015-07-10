4d78f5d2caca5d40cb428b209e56045587074ad6
Raymond Sapida <ray.sapida@gmail.com>
2015-07-10 10:03:48 -0700

    Add order history through sales and purchases actions

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,6 @@ Rails.application.routes.draw do
   get 'pages/about'
   get 'pages/contact'
   get 'seller' => "listings#seller"
+  get 'sales' => "orders#sales"
+  get 'purchases' => "orders#purchases"
 end
