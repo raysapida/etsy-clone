440d04dbeaa8b3fc93cbb3540248fe4fa6349747
Raymond Sapida <ray.sapida@gmail.com>
2015-07-10 09:03:02 -0700

    Nest order routes in listings and update code to using listings when creating an order
    - Edit order form to use similar markup to listing form.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  resources :orders
   devise_for :users
   root 'listings#index'
-  resources :listings
+  resources :listings do
+    resources :orders
+  end
   get 'pages/about'
   get 'pages/contact'
   get 'seller' => "listings#seller"
