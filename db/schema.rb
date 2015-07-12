0149de2459b0e6b0d6bcaa5026879fe7139826d8
Raymond Sapida <ray.sapida@gmail.com>
2015-07-11 20:12:12 -0700

    Add recipents field to user and use it in listings controller
    - Stripe::Recipient.create function is only called when the current user does
      not have a recipient and this is reflected in the form, controller and js.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150710154204) do
+ActiveRecord::Schema.define(version: 20150712030331) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -54,6 +54,7 @@ ActiveRecord::Schema.define(version: 20150710154204) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.string   "name"
+    t.string   "recipient"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
