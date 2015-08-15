3421f826191422abd55cd4baea8d778d855f9fdc
Raymond Sapida <ray.sapida@gmail.com>
2015-08-15 03:05:10 -0700

    Add tests for associations between user and sales and purchases

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,4 +3,6 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   it { should validate_presence_of :name }
   it { should have_many(:listings).dependent(:destroy) }
+  it { should have_many(:sales).class_name('Order').with_foreign_key(:seller_id) }
+  it { should have_many(:purchases).class_name('Order').with_foreign_key(:buyer_id) }
 end
