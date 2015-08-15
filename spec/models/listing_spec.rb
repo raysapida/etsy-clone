371f70786da82cb8b86e6682a50716a3964e2c85
Raymond Sapida <ray.sapida@gmail.com>
2015-08-15 03:07:44 -0700

    Add tests for associations in order and listing models

--- spec/models/listing_spec.rb
+++ spec/models/listing_spec.rb
@@ -6,4 +6,5 @@ RSpec.describe Listing, type: :model do
   it { should validate_presence_of :price }
   it { should validate_numericality_of(:price).is_greater_than(0) }
   it { should belong_to :user }
+  it { should have_many :orders }
 end
--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -5,4 +5,6 @@ RSpec.describe Order, type: :model do
   it { should validate_presence_of :city }
   it { should validate_presence_of :state }
   it { should belong_to :listing }
+  it { should belong_to(:buyer).class_name('User') }
+  it { should belong_to(:seller).class_name('User') }
 end
